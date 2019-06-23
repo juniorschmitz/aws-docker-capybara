853c12ad0bbe3964d1f506b67e696264efd4b757
juniorschmitz <juniorschmitz9@gmail.com>
2019-06-23 18:53:33 -0300

    add: screenshots at the end of the scenarios

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -5,3 +5,7 @@ Before do
   page.current_window.resize_to(1920, 1080)
 end
   
+After do |scenario|
+  screenshot = page.save_screenshot("log/screenshots/#{scenario.__id__}.png")
+  embed(screenshot, "image/png", "Screenshot")
+end
